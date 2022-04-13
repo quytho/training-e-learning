2c6a9f6f9b18c31239fb41888c187b78f101e9e7
TruongNQM <truong29102000@gmail.com>
2022-04-13 16:16:03 +0700

    Show-Update-profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,9 @@
 class UsersController < ApplicationController
+  before_action :get_user, only:[:show, :edit, :update]
   def new
     @user = User.new
   end
+  
   def create 
     @user = User.new(user_params) 
     if @user.save
@@ -9,15 +11,34 @@ class UsersController < ApplicationController
       flash[:success] = "Success!"
       redirect_to user_path(@user)
     else 
-      render 'new'
+      render :new
     end
   end
+
   def show 
-    @user = User.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @user.update user_params
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render :edit
+    end
   end
 
   private 
     def user_params 
-      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :password, :password_confirmation, :image)
+    end
+
+    def get_user
+      @user = User.find_by_id(params[:id])
+      return if @user
+      flash[:danger] = "User not found"
+      redirect_to root_path
     end
 end
