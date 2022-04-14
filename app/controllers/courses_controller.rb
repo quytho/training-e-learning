93ed2813d9542f78fefd633e839b93f728cdb904
Toanlee <leminhtoan1@dtu.edu.vn>
2022-04-14 13:58:15 +0700

    update show course when not found id

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,21 @@
 class CoursesController < ApplicationController
+  before_action :get_course, only: [:show]
+
   def index
     @courses = Course.all
   end
 
   def show
-    @course = Course.find(params[:id])
-    @lessons = @course.lessons
   end
+
+  private
+    def get_course
+      @course = Course.find_by_id(params[:id])
+      if @course.present?
+        return @course
+      else
+        flash[:danger] = "Could not found this course"
+        redirect_to courses_path
+      end
+    end
 end
--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,6 +1,17 @@
 class WordsController < ApplicationController
+  before_action :get_words, only: [:show]
+  
   def show
-    @lesson = Lesson.find(params[:lesson_id])
-    @words = @lesson.words
   end
+  
+  private
+    def get_words
+      @lesson = Lesson.find_by_id(params[:lesson_id])
+      if @lesson.present?
+        return @lesson
+      else
+        flash[:danger] = "The lesson is not available"
+        redirect_to courses_path
+      end
+    end
 end
