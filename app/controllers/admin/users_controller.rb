39a5e161392d692925efa1c9fa5e203972efce81
TruongNQM <truong29102000@gmail.com>
2022-04-18 16:56:25 +0700

    Admin Users

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,11 +1,22 @@
 class Admin::UsersController < ApplicationController
   layout 'layouts/admin'
+  before_action :get_user, only:[:destroy]
+
   def index
-    @users = User.all
+    @users = User.paginate(page: params[:page], per_page: 10)
   end
+
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     redirect_to admin_users_path
   end
+
+  private
+
+    def get_user
+      @user = User.find_by_id(params[:id])
+      return if @user
+      flash[:danger] = "User not found"
+      redirect_to admin_users_path
+    end
 end
--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,10 +1,11 @@
 class AdminController < ApplicationController
-	before_action :check_permission, only: [:index]
-	def index
-	end
+  before_action :check_permission, only: [:index]
 
-	private
-		def check_permission
-			redirect_to login_path if current_user.blank?
-		end
+  def index
+  end
+
+  private
+    def check_permission
+      redirect_to login_path if current_user.blank?
+    end
 end
